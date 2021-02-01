97acacb7e874c04e5a03810f3c4e807c8b058d63
Cathy Tang <cathy.t868@gmail.com>
2021-01-31 18:42:16 -0800

    added mad libs

--- des157/studio1/script.js
+++ des157/studio1/script.js
@@ -1,25 +1,97 @@
 (function(){
     "use strict";
 
+    //canvas stuff
+
+    let isDrawing = false;
+    let x = 0;
+    let y = 0;
+
+    const canvas = document.getElementById('myCanvas');
+    const context = canvas.getContext('2d');
+    const clearBtn = document.getElementById('clearBtn');
+
+    canvas.addEventListener('mousedown', event => {
+        x = event.offsetX;
+        y = event.offsetY;
+        isDrawing = true;
+    });
+      
+    canvas.addEventListener('mousemove', event => {
+    if (isDrawing === true) {
+        drawLine(context, x, y, event.offsetX, event.offsetY);
+        x = event.offsetX;
+        y = event.offsetY;
+    }
+    });
+    
+    window.addEventListener('mouseup', event => {
+    if (isDrawing === true) {
+        drawLine(context, x, y, event.offsetX, event.offsetY);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+    }
+    });
+
+    clearBtn.addEventListener('mousedown', event => {
+        clearCanvas();
+    });
+
+    function drawLine(context, x1, y1, x2, y2) {
+        context.beginPath();
+        context.strokeStyle = '#2c13e1';
+        context.lineWidth = 3;
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.stroke();
+        context.closePath();
+    }
+
+    function clearCanvas() {
+        context.clearRect(0,0,canvas.width,canvas.height);
+    }
+
+    //formstuff
+
     const form = document.querySelector("#myForm");
     const story = document.querySelector("#story");
     form.addEventListener("submit", function(event){
         event.preventDefault();
-        const noun1 = document.querySelector("#noun1").value;
-        const noun2 = document.querySelector("#noun2").value;
-        const adj1 = document.querySelector("#adj1").value;
-        const verb1 = document.querySelector("#verb1").value;
+        const fruit = document.querySelector("#fruit").value;
+        const veg = document.querySelector("#veg").value;
+        const protein = document.querySelector("#protein").value;
+        const dish = document.querySelector("#dish").value;
+        const drink = document.querySelector("#drink").value;
 
-        if (noun1 && noun2 && adj1 && verb1) {
-            story.innerHTML = `Here are your words: ${noun1}, ${noun2}, ${adj1}, ${verb1}`;
+        if (fruit && veg && protein && dish && drink) {
+            //show overlay
+            document.getElementById("overlay").className = "showing";
+
+            story.innerHTML = `Let's make ${dish}! <br> We can add ${fruit}, ${veg}, ${protein}, and a splash of ${drink} to it. Since those are all our
+            favorite ingredients, it should taste great! 🤩`;
+            document.getElementById("story").style.padding = "40px 60px 50px 50px";
         } else {
             alert("Fill out all of the fields to get your Mad Lib!");
         }
 
         // clear fields
-        document.querySelector("#noun1").value = "";
-        document.querySelector("#noun2").value = "";
-        document.querySelector("#adj1").value = "";
-        document.querySelector("#verb1").value = "";
+        document.querySelector("#fruit").value = "";
+        document.querySelector("#veg").value = "";
+        document.querySelector("#protein").value = "";
+        document.querySelector("#dish").value = "";
+        document.querySelector("#drink").value = "";
+    });
+
+    //more overlay stuff
+    document.querySelector(".close").addEventListener("click", function(){
+        event.preventDefault();
+        document.getElementById("overlay").className = "hidden";
+    });
+
+    document.addEventListener('keydown', function(event){
+        if (event.key === "Escape") {
+            document.getElementById("overlay").className = "hidden";
+        }
     });
 }());
