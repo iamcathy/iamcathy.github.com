2df5bbde8b8c9ecab8895f5cc077a56193a59c0e
Cathy Tang <cathy.t868@gmail.com>
2021-02-16 14:45:22 -0800

    added footer

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -3,11 +3,11 @@
     console.log("reading js");
 
     const navLinks = document.querySelectorAll('nav ul li a');
-    
+    //gets each nav link
     navLinks.forEach(function(eachLink){
         eachLink.addEventListener('click', smoothScroll);
     });
-
+    //smooth scrolling function
     function smoothScroll(event) {
         event.preventDefault();
         
@@ -19,29 +19,3 @@
         console.log(originalTop);
     } 
 })();
-    
-window.addEventListener("load", function(){
-    const navLinks = document.querySelectorAll("nav ul li a");
-    const posts = document.querySelectorAll("section");
-    let postTops = [];
-    let pagetop;
-    let counter = 1;
-    let prevCounter = 1;
-
-    window.addEventListener("scroll", function() {
-        pagetop = window.pageYOffset + 250;
-
-        if (pagetop > postTops[counter]) {
-            counter++;
-            console.log(`scrolling down ${counter}`);
-        } else if (counter > 1 && pagetop < postTops [counter - 1]) {
-            counter--;
-            console.log(`scrolling up ${counter}`);
-        }
-        if (counter != prevCounter) {
-            navLinks.forEach(function(eachLink) {
-                eachLink.removeAttribute("class");
-            });
-        }
-    });
-});
