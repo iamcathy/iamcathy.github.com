0d03cabe9f200042aa7d43f8ba4c04aee34b2f31
Cathy Tang <cathy.t868@gmail.com>
2021-03-11 01:14:03 -0800

    update final proj

--- des157/final/final_proj/script.js
+++ des157/final/final_proj/script.js
@@ -10,8 +10,8 @@
     const player2 = document.querySelector("#name2").value;
     const playerone = document.getElementById("playerone");
     const playertwo = document.getElementById("playertwo");
-    const piece1 = document.getElementsByClassName("piece1");
-    const piece2 = document.getElementsByClassName("piece2");
+    let piece1 = document.getElementById("piece1");
+    let piece2 = document.getElementById("piece2");
     
     const bgSound = new Audio("media/jazz.mp3");
     const yay = new Audio("media/yay.mp3");
@@ -23,12 +23,10 @@
         players: ["player1", "player2"],
         score: [0, 0],
         roll1: 0,
-        roll2: 0,
         rollSum:0,
         index: 0,
         gameEnd: 12
     };
-
 
     //show overlay
     const gameTitle = document.querySelector("#gametitle");
@@ -65,7 +63,7 @@
     });
 
     function setUpTurn() {
-        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        game.innerHTML = `<h4>Roll the dice for the ${gameData.players[gameData.index]}</h4>`;
         console.log(gameData.index);
         if (gameData.index == 0) {
             document.getElementById("actions").className = "actionsleft";
@@ -87,44 +85,157 @@
         diceSound.play();
         actionArea.innerHTML = "";
         gameData.roll1 = Math.floor(Math.random() * 3) + 1;
-        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        game.innerHTML = `<h4>Roll the dice for the ${gameData.players[gameData.index]}</h4>`;
         game.innerHTML += `<img src = "${gameData.dice[gameData.roll1-1]}">`;
-        gameData.rollSum = gameData.roll1;
         console.log(gameData);
 
-        if ( gameData.roll1 === 1 ) {
-            game.innerHTML += "<p>Your piece moves one space!</p>";
-            /* if (gameData.rollSum === 1) {
-                piece1.css('grid-column-start') = 1;
-                piece1.css('grid-column-start') = 2;
-            } */
-            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            showCurrentScore();
-            setTimeout(setUpTurn, 2000);
-        } else if ( gameData.roll1 === 1 || gameData.roll2 === 1 ) {
+        if ( gameData.roll1 === 3) {
             wah.play();
+            if (gameData.score[gameData.index] === 0) {
+                showCurrentScore()
+                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                game.innerHTML += `<p id = "one">Sorry, you rolled a three,<br> switching
+                        to ${gameData.players[gameData.index]}</p>`
+                setTimeout(setUpTurn, 2000);
+            } else {
+                console.log("uwu");
+                console.log(gameData.score[gameData.index]);
+                gameData.score[gameData.index] = gameData.score[gameData.index] - 1;
+                showCurrentScore()
+                if (gameData.index === 0) {
+                    if (gameData.score[0] === 1) {
+                        piece1.className = "piece1 two";
+                    } else if (gameData.score[0] === 0) {
+                        piece1.className = "piece1 one";
+                    } else if (gameData.score[0] === 2) {
+                        piece1.className = "piece1 three";
+                    } else if (gameData.score[0] === 3) {
+                        piece1.className = "piece1 four";
+                    } else if (gameData.score[0] === 4) {
+                        piece1.className = "piece1 five";
+                    } else if (gameData.score[0] === 5) {
+                        piece1.className = "piece1 six";
+                    } else if (gameData.score[0] === 6) {
+                        piece1.className = "piece1 seven";
+                    } else if (gameData.score[0] === 7) {
+                        piece1.className = "piece1 eight";
+                    } else if (gameData.score[0] === 8) {
+                        piece1.className = "piece1 nine";
+                    } else if (gameData.score[0] === 9) {
+                        piece1.className = "piece1 ten";
+                    } else if (gameData.score[0] === 10) {
+                        piece1.className = "piece1 eleven";
+                    } else if (gameData.score[0] === 11) {
+                        piece1.className = "piece1 twelve";
+                    } else if (gameData.score[0] === 12) {
+                        piece1.className = "piece1 thirteen";
+                    }
+                } else if (gameData.index === 1) {
+                    if (gameData.score[1] === 1) {
+                        piece2.className = "piece2 two";
+                    } else if (gameData.score[1] === 0) {
+                        piece2.className = "piece2 one";
+                    } else if (gameData.score[1] === 2) {
+                        piece2.className = "piece2 three";
+                    } else if (gameData.score[1] === 3) {
+                        piece2.className = "piece2 four";
+                    } else if (gameData.score[1] === 4) {
+                        piece2.className = "piece2 five";
+                    } else if (gameData.score[1] === 5) {
+                        piece2.className = "piece2 six";
+                    } else if (gameData.score[1] === 6) {
+                        piece2.className = "piece2 seven";
+                    } else if (gameData.score[1] === 7) {
+                        piece2.className = "piece2 eight";
+                    } else if (gameData.score[1] === 8) {
+                        piece2.className = "piece2 nine";
+                    } else if (gameData.score[1] === 9) {
+                        piece2.className = "piece2 ten";
+                    } else if (gameData.score[1] === 10) {
+                        piece2.className = "piece2 eleven";
+                    } else if (gameData.score[1] === 11) {
+                        piece2.className = "piece2 twelve";
+                    } else if (gameData.score[1] === 12) {
+                        piece2.className = "piece2 thirteen";
+                    }
+                }
+                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                game.innerHTML += `<p id = "one">Oh no! You rolled a three,<br>going back one space</p>`
+                setTimeout(setUpTurn, 2000);
+            }
+        } else if ( gameData.roll1 === 2) {
+            wah.play();
+            showCurrentScore()
             gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            game.innerHTML += `<p id = "one">Sorry, one of your rolls was a one,<br> switching
+            game.innerHTML += `<p id = "one">Sorry, you didn't roll a one,<br> switching
                         to ${gameData.players[gameData.index]}</p>`
             setTimeout(setUpTurn, 2000);
-        } else {
-            gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-            actionArea.innerHTML = '<button id = "rollagain">Roll again</button><button id = "pass">Pass</button>';
+        } else if ( gameData.roll1 === 1 ) {
+            console.log(`gameData.roll1`);
+            console.log(gameData.rollSum);
+            gameData.rollSum = gameData.roll1;
+            game.innerHTML += "<p>Your piece moves one space!</p>";
+            gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.roll1;
+            if (gameData.score[0] === 1) {
+                piece1.className = "piece1 two";
+            } else if (gameData.score[0] === 2) {
+                piece1.className = "piece1 three";
+            } else if (gameData.score[0] === 3) {
+                piece1.className = "piece1 four";
+            } else if (gameData.score[0] === 4) {
+                piece1.className = "piece1 five";
+            } else if (gameData.score[0] === 5) {
+                piece1.className = "piece1 six";
+            } else if (gameData.score[0] === 6) {
+                piece1.className = "piece1 seven";
+            } else if (gameData.score[0] === 7) {
+                piece1.className = "piece1 eight";
+            } else if (gameData.score[0] === 8) {
+                piece1.className = "piece1 nine";
+            } else if (gameData.score[0] === 9) {
+                piece1.className = "piece1 ten";
+            } else if (gameData.score[0] === 10) {
+                piece1.className = "piece1 eleven";
+            } else if (gameData.score[0] === 11) {
+                piece1.className = "piece1 twelve";
+            } else if (gameData.score[0] === 12) {
+                piece1.className = "piece1 thirteen";
+            }
 
-            document.getElementById("rollagain").addEventListener("click", function (){
-                setUpTurn();
-            });
-
-            document.getElementById("pass").addEventListener("click", function(){
-                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                setUpTurn();
-            });
+            if (gameData.score[1] === 1) {
+                piece2.className = "piece2 two";
+            } else if (gameData.score[1] === 2) {
+                piece2.className = "piece2 three";
+            } else if (gameData.score[1] === 3) {
+                piece2.className = "piece2 four";
+            } else if (gameData.score[1] === 4) {
+                piece2.className = "piece2 five";
+            } else if (gameData.score[1] === 5) {
+                piece2.className = "piece2 six";
+            } else if (gameData.score[1] === 6) {
+                piece2.className = "piece2 seven";
+            } else if (gameData.score[1] === 7) {
+                piece2.className = "piece2 eight";
+            } else if (gameData.score[1] === 8) {
+                piece2.className = "piece2 nine";
+            } else if (gameData.score[1] === 9) {
+                piece2.className = "piece2 ten";
+            } else if (gameData.score[1] === 10) {
+                piece2.className = "piece2 eleven";
+            } else if (gameData.score[1] === 11) {
+                piece2.className = "piece2 twelve";
+            } else if (gameData.score[1] === 12) {
+                piece2.className = "piece2 thirteen";
+            }
+            showCurrentScore();
             checkWinningCondition();
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+            setTimeout(setUpTurn, 2000);
         }
     }
 
     function checkWinningCondition(){
-        if(gameData.score[gameData.index] > gameData.gameEnd) {
+        if(gameData.score[gameData.index] === gameData.gameEnd) {
             yay.play();
             score.innerHTML = `<p>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</p>`;
             document.getElementById("game").className = "hidden";
@@ -136,6 +247,17 @@
     }
     
     function showCurrentScore() {
-        score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]}: ${gameData.score[0]}</strong> and <strong>${gameData.players[1]}: ${gameData.score[1]}</strong></p>`;
+        if (gameData.roll1 === 1) {
+            score.innerHTML = `<p><strong>${gameData.players[gameData.index]} moves one space forward!</strong></p>`;
+        } else if (gameData.roll1 === 2) {
+            score.innerHTML = `<p><strong>Aww! ${gameData.players[gameData.index]} rolled a two! Switching players . . .</strong></p>`;
+        } else if  (gameData.roll1 === 3) {
+            score.innerHTML = `<p><strong>Oh no! ${gameData.players[gameData.index]} rolled a three! Go back one space.</strong></p>`;
+        }
+        setTimeout(defaultScore, 2500);
+    }
+
+    function defaultScore() {
+        score.innerHTML = `<h2><strong>GAME OF CAT</strong></h2>`;
     }
 })();
